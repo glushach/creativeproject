18ea28f30bbd10cc776aab61a961898d59a3edb2
Aleksandr <shoppingplaneta@gmail.com>
2020-12-01 00:40:33 +0200

    doing slider in section services

--- app/js/script.js
+++ app/js/script.js
@@ -17,7 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
 
-
+  //HERO SLIDER
   const singleTrigger = document.querySelectorAll('.dots__single'),
         dotsSingular = document.querySelectorAll('.dots__singular'),
         dotsSingle = document.querySelectorAll('#single')[0],
@@ -74,18 +74,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
     dotsSingle.addEventListener('click', (e) => {
       for(let i = 0; i < singleTrigger.length + 1; i++) {
-        if(e.target.classList.contains('dots__item') && e.target == singleTrigger[i - 1]){
+        if(e.target.classList.contains('dots__single') && e.target == singleTrigger[i - 1]){
           currentSlide(i);
         }
       }
       for(let i = 0; i < dotsSingular.length + 1; i++) {
-        if(e.target.classList.contains('dots__inner') && e.target == dotsSingular[i - 1]){
+        if(e.target.classList.contains('dots__singular') && e.target == dotsSingular[i - 1]){
           currentSlide(i);
         }
       }
     });//end event
-
-
+   
 
   const tabs = document.querySelectorAll('.tabs__click'),
         contents = document.querySelectorAll('.contents'),
@@ -126,6 +125,68 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+
+
+  //SLIDER TWO
+  const slideItem = document.querySelectorAll('.slides__inner'),
+        dotsMultiple = document.querySelectorAll('#multiple')[0],
+        dotsExternal = document.querySelectorAll('.dots__multiple'),
+        dotsInner = document.querySelectorAll('.dots__plural');
+  let IndexOfSlideItem = 1,
+      IndexOfSlideTwo = 2;
+
+
+  if(window.screen.availWidth <= 1100) {
+    function showSlideItem (n) {
+      if(n < 1) {
+        IndexOfSlideItem = slideItem.length;
+      } else if (n > slideItem.length) {
+        IndexOfSlideItem = 1;
+      }
+
+      for(let i = 0; i < slideItem.length; i++) {
+        slideItem[i].style.display = 'none';
+        slideItem[i].classList.remove('slides__inner_active');
+      }
+
+
+      for(let i = 0; i < dotsExternal.length; i++) {
+        dotsExternal[i].classList.remove('dots__multiple_active');
+      }
+      for(let i = 0; i < dotsInner.length; i++) {
+        dotsInner[i].classList.remove('dots__plural_active');
+      }
+
+        //ЭТА ЧАСТЬ КОДУ УСТАНАВЛИВАЕТ БЛОЧНОСТЬ СЛАЙДОВ
+        
+      slideItem[IndexOfSlideItem - 1].style.display = 'block';
+      slideItem[IndexOfSlideItem - 1].classList.add('slides__inner_active');
+      dotsExternal[IndexOfSlideItem - 1].classList.add('dots__multiple_active');
+      dotsInner[IndexOfSlideItem - 1].classList.add('dots__plural_active');
+    }//end fn showSlides
+
+    showSlideItem(slideIndex);
+
+
+    function currentSlideItem(n) {
+      showSlideItem(IndexOfSlideItem = n);
+    }
+
+    dotsMultiple.addEventListener('click', (e) => {
+      console.log('Click');
+      for(let i = 0; i < dotsExternal.length + 1; i++) {
+        if(e.target.classList.contains('dots__multiple') && e.target == dotsExternal[i - 1]){
+          currentSlideItem(i);
+        }
+      }
+      for(let i = 0; i < dotsInner.length + 1; i++) {
+        if(e.target.classList.contains('dots__plural') && e.target == dotsInner[i - 1]){
+          currentSlideItem(i);
+        }
+      }
+    });//end event
+  }
 
 
 });
